c259fdf4e546bd27d5c9880d542e18f2f1077a54
Jamy <jamy.bailly.dev@gmail.com>
2023-12-03 17:17:46 +0100

    Add UiKit Sample

---
+++ sample/VerifyUiKitSample/VerifyUiKitSample/VerifyViewController.swift
@@ -0,0 +1,28 @@
+//
+//  VerifyViewController.swift
+//  VerifyUiKitSample
+//
+//  Created by Jamy Bailly on 03/12/2023.
+//
+
+import UIKit
+import SynapsVerify
+
+class VerifyViewController : UIViewController {
+    @IBOutlet weak var verifyView: VerifyUiView!
+
+    var sessionId: String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        verifyView.startSession(sessionId: sessionId, lang: .french)
+        verifyView.onReady {
+            print("onReady")
+        }
+        verifyView.onFinished {
+            print("onFinish")
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+}
